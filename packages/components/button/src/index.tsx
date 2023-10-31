45f11fe91c0afa5cc94943c4f51480a1e42503c6
Abel Lopes <abelflopes@gmail.com>
2023-10-31 23:28:58 +0000

    feat: support icon in button

--- packages/components/button/src/index.tsx
+++ packages/components/button/src/index.tsx
@@ -4,15 +4,18 @@ import classNames from "classnames";
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   skin?: "primary" | "secondary";
+  icon?: React.ReactNode;
 }
 
 export const Button = ({
   skin = "primary",
+  icon,
   children,
   className,
   ...otherProps
 }: Readonly<ButtonProps>): React.ReactElement => (
   <button className={classNames(styles.root, styles[skin], className)} {...otherProps}>
+    {icon && <span className={styles.icon}>{icon}</span>}
     {children}
   </button>
 );
