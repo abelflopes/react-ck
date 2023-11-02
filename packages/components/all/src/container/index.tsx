038a1c972b142af1ed13618f7da96be63050cdb9
Abel Lopes <abelflopes@gmail.com>
2023-11-02 23:26:07 +0000

    refactor: simplify variation & spacing declaration

--- packages/components/all/src/container/index.tsx
+++ packages/components/all/src/container/index.tsx
@@ -2,25 +2,31 @@ import React from "react";
 import styles from "./index.module.scss";
 import classNames from "classnames";
 
-type ContainerVariation = "horizontal" | "vertical" | "small";
+type ContainerVariation = "small";
 
-interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
   variation?: ContainerVariation | ContainerVariation[];
+  spacingX?: boolean;
+  spacingY?: boolean;
 }
 
 export const Container = ({
-  variation = "horizontal",
+  variation,
+  spacingX = true,
+  spacingY,
   className,
   ...otherProps
 }: Readonly<ContainerProps>): React.ReactElement => {
-  const computedVariations = Array.isArray(variation) ? variation : [variation];
+  const computedVariations =
+    Array.isArray(variation) || variation === undefined ? variation : [variation];
 
   return (
     <div
       className={classNames(
         styles.root,
+        { [`${styles.horizontal}`]: spacingX, [`${styles.vertical}`]: spacingY },
+        computedVariations?.map((i) => styles[i]),
         className,
-        computedVariations.map((i) => styles[i]),
       )}
       {...otherProps}
     />
