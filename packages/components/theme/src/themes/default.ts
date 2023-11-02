b91da4ca3fa10d7e32b3e9d37fd6b27374f4e433
Abel Lopes <abelflopes@gmail.com>
2023-11-02 10:48:03 +0000

    feat: update theme neutral colors

--- packages/components/theme/src/themes/default.ts
+++ packages/components/theme/src/themes/default.ts
@@ -1,12 +1,17 @@
 import type { Theme } from "../types";
 
+// colorPrimary: "#783192",
+// colorSecondary: "#59BCC8",
+// textColor: "#1E2232",
+
 export const defaultTheme: Theme = {
   color: {
-    highlightPrimary: "#9e51f0",
+    highlightPrimary: "#59BCC8",
     highlightSecondary: "#3e86d4",
     text: "#1E2232",
     textAlt: "#323646",
-    neutral100: "#DBDEEB",
+    neutral100: "#edf2f7",
+    neutral200: "#DBDEEB",
   },
   font: {
     family: "system-ui, sans-serif",
