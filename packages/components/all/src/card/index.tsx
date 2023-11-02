ffdca126c176275d97e1dc8236811905dfa16217
Abel Lopes <abelflopes@gmail.com>
2023-11-02 10:44:42 +0000

    feat: add card component

---
+++ packages/components/all/src/card/index.tsx
@@ -0,0 +1,13 @@
+import classNames from "classnames";
+import styles from "./index.module.scss";
+import React from "react";
+
+export const Card = ({
+  children,
+  className,
+  ...otherProps
+}: Readonly<React.HTMLAttributes<HTMLDivElement>>): React.ReactElement => (
+  <div {...otherProps} className={classNames(styles.root, className)}>
+    {children}
+  </div>
+);
