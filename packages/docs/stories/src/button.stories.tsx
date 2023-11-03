943212a2567194507518319beab3eaf78c0f6346
Abel Lopes <abelflopes@gmail.com>
2023-11-03 02:14:52 +0000

    refactor: use faker in storybook

--- packages/docs/stories/src/button.stories.tsx
+++ packages/docs/stories/src/button.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Button } from "@rck/button/src/index";
 import type { Meta, StoryObj } from "@storybook/react";
 import { ThemeProvider } from "@rck/theme";
+import { faker } from "@faker-js/faker";
+import { sentenceCase } from "change-case";
+import { Button } from "@rck/button/src/index";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
@@ -26,13 +28,13 @@ type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
   args: {
-    children: "Submit",
+    children: sentenceCase(faker.lorem.words({ min: 1, max: 3 })),
   },
 };
 
 export const Secondary: Story = {
   args: {
     skin: "secondary",
-    children: "Cancel",
+    children: sentenceCase(faker.lorem.words({ min: 1, max: 3 })),
   },
 };
--- packages/docs/stories/src/input.stories.tsx
+++ packages/docs/stories/src/input.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Input } from "@rck/input/src/index";
 import type { Meta, StoryObj } from "@storybook/react";
 import { ThemeProvider } from "@rck/theme";
+import { faker } from "@faker-js/faker";
+import { sentenceCase } from "change-case";
+import { Input } from "@rck/input/src/index";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
@@ -27,6 +29,6 @@ type Story = StoryObj<typeof Input>;
 export const Default: Story = {
   args: {
     required: true,
-    placeholder: "Username",
+    placeholder: sentenceCase(faker.lorem.words({ min: 1, max: 2 })),
   },
 };
