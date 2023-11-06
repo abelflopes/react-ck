787d6de16ab6188bbeb118a249528aafa63d3465
Abel Lopes <abelflopes@gmail.com>
2023-11-06 18:57:43 +0000

    docs: add component demos

---
+++ packages/docs/stories/src/banner.stories.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { ThemeProvider } from "@rck/theme";
+import { faker } from "@faker-js/faker";
+import { capitalCase } from "change-case";
+import { Banner, Text } from "@rck/all/src/index";
+import { Button } from "@rck/button/src/index";
+
+const meta: Meta<typeof Banner> = {
+  title: "Components/Banner",
+  component: Banner,
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Banner>;
+
+export const WithText: Story = {
+  args: {
+    src: faker.image.urlPicsumPhotos(),
+    children: (
+      <>
+        <Text type="huge">{faker.lorem.sentence()}</Text>
+        <Text>{faker.lorem.sentence()}</Text>
+      </>
+    ),
+  },
+};
+
+export const ImageOnly: Story = {
+  args: {
+    src: faker.image.urlPicsumPhotos(),
+  },
+};
+
+export const WithActions: Story = {
+  args: {
+    src: faker.image.urlPicsumPhotos(),
+    children: (
+      <>
+        <Text type="huge">{faker.lorem.sentence()}</Text>
+        <Text>{faker.lorem.sentence()}</Text>
+      </>
+    ),
+    actions: [
+      <Button key="1">{capitalCase(faker.lorem.word())}</Button>,
+      <Button key="2" skin="secondary">
+        {capitalCase(faker.lorem.words({ min: 1, max: 2 }))}
+      </Button>,
+    ],
+  },
+};
---
+++ packages/docs/stories/src/container.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { ThemeProvider } from "@rck/theme";
+import { faker } from "@faker-js/faker";
+import { Container, Text } from "@rck/all/src/index";
+
+const meta: Meta<typeof Container> = {
+  title: "Components/Container",
+  component: Container,
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Container>;
+
+export const Default: Story = {
+  args: {
+    spacingX: true,
+    spacingY: true,
+    variation: "big",
+    children: <Text>{faker.lorem.sentence({ min: 50, max: 200 })}</Text>,
+  },
+};
