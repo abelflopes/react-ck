584e4c253b845171260babafa7971bfdc1a6ca19
Abel Lopes <abelflopes@gmail.com>
2023-11-08 01:08:06 +0000

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 /** @type import("eslint").Linter.Config */
 const config = {
   extends: "@abelflopes/eslint-config-tsr-pro",
+  rules: {
+    "import/no-unresolved": 0,
+    "unicorn/prefer-spread": 0,
+    "unicorn/prevent-abbreviations": 0,
+    "unicorn/no-array-reduce": 0,
+  },
 };
 
 module.exports = config;
