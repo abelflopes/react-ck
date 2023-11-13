17757ad77557dca7331d93d3a46cf7330ca83864
Abel Lopes <abelflopes@gmail.com>
2023-11-13 14:09:03 +0000

    feat: add social icons

--- packages/components/icon/src/icons.ts
+++ packages/components/icon/src/icons.ts
@@ -1,8 +1,14 @@
 // Icons - https://react-icons.github.io/react-icons/icons?name=si
 import { BsBellFill } from "react-icons/bs";
 import { GrClose } from "react-icons/gr";
+import { SiLinkedin, SiCodersrank, SiNpm, SiCodepen, SiGithub } from "react-icons/si";
 
 export default {
   "bell-filled": BsBellFill,
   "close": GrClose,
+  "linkedin": SiLinkedin,
+  "codersrank": SiCodersrank,
+  "npm": SiNpm,
+  "codepen": SiCodepen,
+  "github": SiGithub,
 };
