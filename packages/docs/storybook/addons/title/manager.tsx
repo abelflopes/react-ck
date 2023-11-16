2709e49f2ffb44d2b5ae8ce973adaf70ae72b551
Abel Lopes <abelflopes@gmail.com>
2023-11-16 23:05:40 +0000

    chore: remove unused imports

--- packages/docs/storybook/addons/title/manager.tsx
+++ packages/docs/storybook/addons/title/manager.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { addons, types } from "@storybook/manager-api";
+import { addons } from "@storybook/manager-api";
 import * as Events from "@storybook/core-events";
 
 addons.register("page-title", (api) => {
