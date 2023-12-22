6ee6289282c1ef3520af46c7e9578bc0dee615b0
Abel Lopes <abelflopes@gmail.com>
2023-12-22 00:36:07 +0000

    docs: add annotations

--- packages/utils/manager/src/index.tsx
+++ packages/utils/manager/src/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { ThemeProvider, type ThemeProviderProps } from "@react-ck/theme";
 import { LayersProvider } from "@react-ck/layers";
 
+/** Props for the Manager component  */
 export interface ManagerProps {
+  /** Properties of the {@link ThemeProvider} */
   theme?: Omit<ThemeProviderProps, "children">;
+  /** Content slot */
   children: React.ReactNode;
 }
 
-// TODO: add annotation
+/**
+ * Manager component for handling themes and layers.
+ * @param props - {@link ManagerProps}
+ * @returns The provided content wrapped by theme & layers providers.
+ */
 export const Manager = ({ theme, children }: Readonly<ManagerProps>): React.ReactElement => (
   <ThemeProvider {...theme}>
     <LayersProvider>{children}</LayersProvider>
