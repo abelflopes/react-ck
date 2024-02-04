12b147f4e418656827cf6486479232b8aed4e443
Abel Lopes <abelflopes@gmail.com>
2024-02-04 01:34:56 +0000

    style: fix ts issues

--- packages/components/data-table/specs/index.snapshot.test.tsx
+++ packages/components/data-table/specs/index.snapshot.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { DataTable, type DataTableProps } from "../src/index";
+import { DataTable } from "../src/index";
 import renderer from "react-test-renderer";
 
 describe("Snapshot DataTable", () => {
   test("renders correctly", async () => {
-    const data: DataTableProps["data"] = [{ a: 1, b: 2, c: 3 }];
+    const data = [{ a: 1, b: 2, c: 3 }];
     const tree = renderer.create(<DataTable data={data} data-testid="table" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
--- packages/components/data-table/specs/index.unit.test.tsx
+++ packages/components/data-table/specs/index.unit.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { DataTable, type DataTableProps } from "../src/index";
+import { DataTable } from "../src/index";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 describe("Unit DataTable", () => {
   test("renders correctly", async () => {
-    const data: DataTableProps["data"] = [{ a: 1, b: 2, c: 3 }];
+    const data = [{ a: 1, b: 2, c: 3 }];
     render(<DataTable data={data} data-testid="table" />);
     const find = await screen.findByTestId("table");
     expect(find).toBeInTheDocument();
