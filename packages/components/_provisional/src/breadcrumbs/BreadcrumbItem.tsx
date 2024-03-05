c2c7fe56351372e13c3e2413df66be4667524339
Abel Lopes <abelflopes@gmail.com>
2024-03-05 03:32:45 +0000

    feat: make breadcrumb polymorphic

--- packages/components/_provisional/src/breadcrumbs/BreadcrumbItem.tsx
+++ packages/components/_provisional/src/breadcrumbs/BreadcrumbItem.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import styles from "./styles/item.module.scss";
 import classNames from "classnames";
+import {
+  PolymorphicComponent,
+  type ConsumerPolymorphicProps,
+  type HTMLTag,
+} from "@react-ck/react-utils";
 
-export interface BreadcrumbItemProps extends React.HTMLAttributes<HTMLElement> {
+export interface BreadcrumbItemProps<T extends HTMLTag>
+  extends React.ButtonHTMLAttributes<HTMLSpanElement>,
+    ConsumerPolymorphicProps<T> {
   active?: boolean;
 }
 
-export const BreadcrumbItem = ({
+export const BreadcrumbItem = <T extends HTMLTag>({
+  as,
   active,
   className,
   children,
   ...otherProps
-}: Readonly<BreadcrumbItemProps>): React.ReactElement => (
-  <span className={classNames(styles.root, active && styles.active, className)} {...otherProps}>
+}: Readonly<BreadcrumbItemProps<T>>): React.ReactElement => (
+  <PolymorphicComponent
+    as={as}
+    fallback={["span", otherProps]}
+    commonProps={{
+      className: classNames(styles.root, active && styles.active, className),
+    }}>
     {children}
-  </span>
+  </PolymorphicComponent>
 );
