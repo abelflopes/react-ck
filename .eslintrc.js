37ea1a2d0841bc490f54dd2355f80792cb605037
Abel Lopes <abelflopes@gmail.com>
2024-03-11 01:51:52 +0000

    chore: add compat plugin to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,20 @@
+/**
+ * Eslint TS pro
+ * https://www.npmjs.com/package/@abelflopes/eslint-config-tsr-pro
+ *
+ * Eslint compat
+ * https://github.com/amilajack/eslint-plugin-compat
+ */
+
 /** @type import("eslint").Linter.Config */
 const config = {
-  extends: "@abelflopes/eslint-config-tsr-pro",
+  extends: ["@abelflopes/eslint-config-tsr-pro", "plugin:compat/recommended"],
+  plugins: ["compat"],
   rules: {
     "import/no-extraneous-dependencies": "off",
+  },
+  settings: {
+    lintAllEsApis: true,
   },
 };
 
