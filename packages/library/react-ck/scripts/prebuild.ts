138a65eab9fc3a7c03d6d51feb692411f7a6a1ee
Abel Lopes <abelflopes@gmail.com>
2024-03-14 21:26:22 +0000

    build: add commit after global package is prebuild

--- packages/library/react-ck/scripts/prebuild.ts
+++ packages/library/react-ck/scripts/prebuild.ts
@@ -155,4 +155,16 @@ fs.writeFileSync(destScssFile, scssFileContents, {
   encoding: "utf-8",
 });
 
+// Add changes to git
+
+logger.info("Adding to git...", currPackagePath);
+
+execSync(`git add ${currPackagePath}`, { stdio: "inherit" });
+
+logger.info("Sending commit to git...", currPackagePath);
+
+execSync(`git commit -m "feat(react-ck): update library packages"`, { stdio: "inherit" });
+
+// Finished
+
 logger.info("Done");
