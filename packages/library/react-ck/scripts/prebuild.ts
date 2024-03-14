ad1e2b1a3dc94e6ad7bdf4dff0af693ee183585d
Abel Lopes <abelflopes@gmail.com>
2024-03-14 22:22:15 +0000

    build: fix library prebuild stuck in git process

--- packages/library/react-ck/scripts/prebuild.ts
+++ packages/library/react-ck/scripts/prebuild.ts
@@ -157,13 +157,17 @@ fs.writeFileSync(destScssFile, scssFileContents, {
 
 // Add changes to git
 
-logger.info("Adding to git...", currPackagePath);
+try {
+  logger.info("Adding to git...", currPackagePath);
 
-execSync(`git add ${currPackagePath}`, { stdio: "inherit" });
+  execSync(`git add ${currPackagePath}`, { stdio: "inherit" });
 
-logger.info("Sending commit to git...", currPackagePath);
+  logger.info("Sending commit to git...", currPackagePath);
 
-execSync(`git commit -m "feat(react-ck): update library packages"`, { stdio: "inherit" });
+  execSync(`git commit -m "feat(react-ck): update library packages"`, { stdio: "inherit" });
+} catch (error) {
+  logger.error(error instanceof Error ? error.message : error);
+}
 
 // Finished
 
