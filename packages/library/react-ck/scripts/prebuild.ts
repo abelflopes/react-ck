27ea573a8c5729697eeda0e3b1321af506a44752
Abel Lopes <abelflopes@gmail.com>
2024-03-17 02:19:12 +0000

    refactor: remove publish scripts from prebuild

--- packages/library/react-ck/scripts/prebuild.ts
+++ packages/library/react-ck/scripts/prebuild.ts
@@ -9,7 +9,6 @@ import {
   logger,
   packagesRoot,
   projectRoot,
-  rootPackageLock,
 } from "./common";
 
 // Util
@@ -152,21 +151,6 @@ fs.mkdirSync(path.dirname(destScssFile), { recursive: true });
 fs.writeFileSync(destScssFile, scssFileContents, {
   encoding: "utf-8",
 });
-
-// Add changes to git
-
-try {
-  logger.info("Adding to git...", currPackagePath);
-
-  execSync(`git add ${rootPackageLock}`, { stdio: "inherit" });
-  execSync(`git add ${currPackagePath}`, { stdio: "inherit" });
-
-  logger.info("Sending commit to git...", currPackagePath);
-
-  execSync(`git commit -m "feat(react-ck): global library package"`, { stdio: "inherit" });
-} catch (error) {
-  logger.error(error instanceof Error ? error.message : error);
-}
 
 // Finished
 
