f60642f4f50912be424b2070ab9cbe34e29d832b
Abel Lopes <abelflopes@gmail.com>
2024-06-07 16:55:36 +0100

    feat: implement tooltip base functionality

--- packages/components/_provisional/src/tooltip/index.tsx
+++ packages/components/_provisional/src/tooltip/index.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import styles from "./index.module.scss";
+import React, { useEffect, useState } from "react";
 import { Layer } from "@react-ck/layers";
 import { PositionEngine, type PositionEngineProps } from "../position-engine";
 import { ScrollableContainer } from "../scrollable-container";
 import { Card } from "@react-ck/card";
-import styles from "./index.module.scss";
+import classNames from "classnames";
 
 export interface TooltipProps {
   anchor: PositionEngineProps["anchorRef"];
   position?: PositionEngineProps["position"];
   children?: React.ReactNode;
+  /**
+   * The tooltip will open/close on hover by default,
+   * if you pass true/false, this behavior will be overridden and should be managed by the consumer
+   */
   open?: boolean;
 }
 
@@ -17,22 +22,58 @@ export const Tooltip = ({
   position = "auto",
   open,
   children,
-}: Readonly<TooltipProps>): React.ReactNode =>
-  open && (
-    <PositionEngine
-      exclude={["left", "right", "end"]}
-      position={position}
-      anchorRef={anchor}
-      render={({ style }) => (
-        <Layer elevation="popup">
-          <div style={style} className={styles.container}>
-            <Card skin="shadowed" spacing="none" className={styles.card}>
-              <ScrollableContainer horizontal={false} className={styles.content}>
-                {children}
-              </ScrollableContainer>
-            </Card>
-          </div>
-        </Layer>
-      )}
-    />
+}: Readonly<TooltipProps>): React.ReactNode => {
+  const [internalOpen, setInternalOpen] = useState(open);
+
+  useEffect(() => {
+    const ref = anchor.current;
+
+    if (open !== undefined) {
+      setInternalOpen(open);
+      return;
+    }
+
+    if (!ref) throw new Error("Tooltip anchor ref is required");
+
+    let to: ReturnType<typeof setTimeout> | undefined = undefined;
+
+    function handleMouseEnter(): void {
+      clearTimeout(to);
+      to = setTimeout(() => {
+        setInternalOpen(true);
+      }, 300);
+    }
+
+    function handleMouseLeave(): void {
+      clearTimeout(to);
+      setInternalOpen(false);
+    }
+
+    ref.addEventListener("mouseenter", handleMouseEnter);
+    ref.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      clearTimeout(to);
+      ref.removeEventListener("mouseenter", handleMouseEnter);
+      ref.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, [anchor, open]);
+
+  return (
+    internalOpen && (
+      <PositionEngine
+        position={position}
+        anchorRef={anchor}
+        render={({ style, position }) => (
+          <Layer elevation="popup">
+            <div style={style} className={classNames(styles.container, position)}>
+              <Card skin="shadowed" spacing="none" className={styles.card}>
+                <div className={styles.content}>{children}</div>
+              </Card>
+            </div>
+          </Layer>
+        )}
+      />
+    )
   );
+};
--- packages/docs/stories/src/tooltip.stories.tsx
+++ packages/docs/stories/src/tooltip.stories.tsx
@@ -4,6 +4,7 @@ import { Manager } from "@react-ck/manager";
 import { configureStory } from "@react-ck/story-config";
 import { Tooltip } from "@react-ck/provisional/src";
 import { Button } from "@react-ck/button";
+import { faker } from "@faker-js/faker";
 
 type Story = StoryObj<typeof Tooltip>;
 
@@ -29,7 +30,8 @@ export default meta;
 
 export const Component: Story = {
   args: {
-    open: true,
+    open: undefined,
+    position: undefined,
   },
   render: ({ open, position }): React.ReactElement => {
     // eslint-disable-next-line react-hooks/rules-of-hooks -- exception for storybook
@@ -39,15 +41,7 @@ export const Component: Story = {
       <>
         <Button rootRef={buttonRef}>Button</Button>
         <Tooltip anchor={buttonRef} open={open} position={position}>
-          Content Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nulla est, quidem
-          enim a molestias accusantium quo officia provident maxime voluptatem, beatae delectus
-          aliquid ipsa perferendis accusamus! Eius, laborum quisquam. Eius soluta deserunt
-          aspernatur tenetur, laudantium quod corrupti natus facilis est ab esse sunt dolore magni
-          cum accusamus nemo. Optio adipisci itaque exercitationem quo nulla, odit eligendi natus
-          est cupiditate aperiam nemo, vero explicabo. Non eveniet ipsum, dolores suscipit sit
-          deserunt doloribus. Dolorum aspernatur iusto, aliquid officiis illo modi vitae.
-          Exercitationem laudantium inventore nemo harum commodi doloribus totam porro aliquam. Quae
-          aliquam iusto neque ipsam non? Consequuntur saepe inventore aliquam!
+          {faker.lorem.sentence(5)}
         </Tooltip>
       </>
     );
