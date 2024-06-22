5666fbcca00ef1e86fdf6f185ca3ba9c6dd65688
Abel Lopes <abelflopes@gmail.com>
2024-06-22 22:46:17 +0100

    chore: better audit display

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -3,34 +3,38 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-echo "\n\n"
-echo "${GREEN}$(pwd) - Running audit${NC}"
+echo "\n"
+echo "----------------------------------------------------------------------------------------"
+echo "${GREEN}Running audit on: $(pwd)${NC}"
+echo "----------------------------------------------------------------------------------------"
 echo "\n"
 
-echo "${YELLOW}$(pwd) - Check dependencies${NC}"
+echo "${YELLOW}Check dependencies${NC}"
 echo "\n"
 npx --yes depcheck
 echo "\n"
 
-echo "${YELLOW}$(pwd) - List TODO's${NC}"
+echo "${YELLOW}List TODO's${NC}"
 npx --yes leasot '{.,*}**' -i '**/node_modules,**/dist,**/packages' -S -x
 echo "\n"
 
-echo "${YELLOW}$(pwd) - Check React CK dependencies${NC}"
+echo "${YELLOW}Check Local dependencies${NC}"
 echo "\n"
 npx --yes npm-check-updates --target latest --filter "@react-ck*"
+echo "\n"
 
-echo "${YELLOW}$(pwd) - Check Project dependencies${NC}"
+echo "${YELLOW}Check External dependencies${NC}"
 echo "\n"
 npx --yes npm-check-updates --target latest --reject "@react-ck*"
+echo "\n"
 
 if [ -f "./tsconfig.json" ]; then
-    echo "${YELLOW}$(pwd) - Check unused with ts-prune${NC}"
+    echo "${YELLOW}Check unused with ts-prune${NC}"
     echo "\n"
     npx --yes ts-prune
     echo "\n"
     
-    echo "${YELLOW}$(pwd) - Check unused with ts-unused-exports${NC}"
+    echo "${YELLOW}Check unused with ts-unused-exports${NC}"
     echo "\n"
     npx --yes ts-unused-exports tsconfig.json --showLineNumber --findCompletelyUnusedFiles --maxIssues=100
     echo "\n"
