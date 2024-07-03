0b726a53823dd2a94cd16e272791498dc1e8fabc
Abel Lopes <abelflopes@gmail.com>
2024-07-03 20:34:34 +0100

    chore: remove unused

--- packages/docs/stories/utils/object-exclude.ts
+++
@@ -1,6 +0,0 @@
-export const objectExclude = <T extends Record<string, unknown>, A extends Array<keyof T>>(
-  obj: T,
-  exc: A,
-): Omit<T, A[number]> =>
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- object entries weak types
-  Object.fromEntries(Object.entries(obj).filter(([k]) => !exc.includes(k))) as Omit<T, A[number]>;
