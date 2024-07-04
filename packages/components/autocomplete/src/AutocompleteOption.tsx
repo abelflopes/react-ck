ef96c56366fbbb511ab28650d8d55487b1a34d23
Abel Lopes <abelflopes@gmail.com>
2024-07-04 22:25:43 +0100

    revert: remove autocomplete component
    autocomplete will be based on existing select

--- packages/components/autocomplete/src/AutocompleteOption.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export type AutocompleteOptionProps = React.OptionHTMLAttributes<HTMLOptionElement>;
-
-export const AutocompleteOption = ({
-  ...props
-}: Readonly<AutocompleteOptionProps>): React.ReactElement => <option {...props} />;
