ac0c987eab29796890924620c122833c9e39c81c
Abel Lopes <abelflopes@gmail.com>
2024-07-04 22:36:29 +0100

    chore: remove unused code

--- packages/components/select/specs/mocks.ts
+++ packages/components/select/specs/mocks.ts
@@ -1,16 +1,3 @@
-type MockFetch<T extends object> = () => Promise<{ json: () => T }>;
-
-function mockResizeObserverImplementation<T extends object>(data: T | Error): MockFetch<T> {
-  return jest.fn().mockImplementation(
-    async (): ReturnType<MockFetch<T>> =>
-      data instanceof Error
-        ? Promise.reject(data)
-        : Promise.resolve({
-            json: () => data,
-          }),
-  );
-}
-
 export const mockResizeObserver = (): void => {
   Object.defineProperty(window, "ResizeObserver", {
     writable: true,
