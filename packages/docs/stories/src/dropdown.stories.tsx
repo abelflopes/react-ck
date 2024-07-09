3745c5fec5c809cc848b57489a946c708f5e2994
Abel Lopes <abelflopes@gmail.com>
2024-07-09 04:14:21 +0100

    docs: add dropdown icon button demo

--- packages/docs/stories/src/dropdown.stories.tsx
+++ packages/docs/stories/src/dropdown.stories.tsx
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { type Meta, type StoryObj } from "@storybook/react";
 import { Manager } from "@react-ck/manager";
 import { configureStory } from "@react-ck/story-config";
 import { Dropdown } from "@react-ck/provisional/src";
-import { Button } from "@react-ck/button";
+import { Button } from "@react-ck/button/src";
+import { Icon } from "@react-ck/icon/src";
+import { IconVerticalDots } from "@react-ck/icon/icons/IconVerticalDots";
 
 type Story = StoryObj<typeof Dropdown>;
 
@@ -39,6 +41,42 @@ export const Component: Story = {
       <>
         <Button rootRef={buttonRef}>Button</Button>
         <Dropdown anchorRef={buttonRef ?? argAnchorRef} {...props}>
+          Content Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nulla est, quidem
+          enim a molestias accusantium quo officia provident maxime voluptatem, beatae delectus
+          aliquid ipsa perferendis accusamus! Eius, laborum quisquam. Eius soluta deserunt
+          aspernatur tenetur, laudantium quod corrupti natus facilis est ab esse sunt dolore magni
+          cum accusamus nemo. Optio adipisci itaque exercitationem quo nulla, odit eligendi natus
+          est cupiditate aperiam nemo, vero explicabo. Non eveniet ipsum, dolores suscipit sit
+          deserunt doloribus. Dolorum aspernatur iusto, aliquid officiis illo modi vitae.
+          Exercitationem laudantium inventore nemo harum commodi doloribus totam porro aliquam. Quae
+          aliquam iusto neque ipsam non? Consequuntur saepe inventore aliquam!
+        </Dropdown>
+      </>
+    );
+  },
+};
+
+export const IconButton: Story = {
+  render: ({ anchorRef: argAnchorRef, ...props }): React.ReactElement => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks -- exception for storybook
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const [open, setOpen] = useState(false);
+
+    return (
+      <>
+        <Button
+          rootRef={buttonRef}
+          skin="ghost"
+          icon={
+            <Icon>
+              <IconVerticalDots />
+            </Icon>
+          }
+          onClick={() => {
+            setOpen((v) => !v);
+          }}
+        />
+        <Dropdown anchorRef={buttonRef ?? argAnchorRef} {...props} open={open || props.open}>
           Content Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nulla est, quidem
           enim a molestias accusantium quo officia provident maxime voluptatem, beatae delectus
           aliquid ipsa perferendis accusamus! Eius, laborum quisquam. Eius soluta deserunt
