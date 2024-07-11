1e474984642c392f9d3f51cddc29af2e6dbc8c06
Abel Lopes <abelflopes@gmail.com>
2024-07-11 01:58:21 +0100

    fix: snackbar hook name

--- packages/components/_provisional/src/snackbar/hook.ts
+++ packages/components/_provisional/src/snackbar/hook.ts
@@ -1,4 +1,4 @@
 import { useContext } from "react";
 import { SnackbarContext, type SnackbarContextProps } from "./context";
 
-export const useSnackbarContext = (): SnackbarContextProps => useContext(SnackbarContext);
+export const useSnackbar = (): SnackbarContextProps => useContext(SnackbarContext);
