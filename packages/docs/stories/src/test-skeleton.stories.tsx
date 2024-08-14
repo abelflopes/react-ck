1ec6022e6ee293198516d170c8733db5b2750db3
Abel Lopes <abelflopes@gmail.com>
2024-08-14 04:00:23 +0100

    docs: add test pages

---
+++ packages/docs/stories/src/test-skeleton.stories.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { type Meta, type StoryObj } from "@storybook/react";
+import { generateDescribedCombinations } from "@react-ck/misc-utils";
+import { Manager } from "@react-ck/manager";
+import { Grid } from "@react-ck/grid/src";
+import { Skeleton } from "@react-ck/skeleton/src";
+import { Text, type TextProps } from "@react-ck/text/src";
+import { capitalCase } from "change-case";
+
+type Story = StoryObj<unknown>;
+
+const meta: Meta = {
+  title: "Test/Skeleton & Text",
+  decorators: [
+    (Story): React.ReactElement => (
+      <Manager>
+        <Story />
+      </Manager>
+    ),
+  ],
+};
+
+export default meta;
+
+export const demo: Story = {
+  parameters: {
+    layout: "padded",
+  },
+  render: (): React.ReactElement => (
+    <Grid align="centered">
+      {generateDescribedCombinations<TextProps<"p">>({
+        variation: ["banner", "h1", "h2", "h3", "h4", "h5", "h6", "p", "small", "extra-small"],
+      }).map((i) => (
+        <React.Fragment key={i.description}>
+          <Grid.Column size={4}>
+            <div style={{ outline: "solid 1px green" }}>
+              <Text {...i.combination}>{capitalCase(i.combination.variation ?? "-")}</Text>
+            </div>
+          </Grid.Column>
+          <Grid.Column size={4}>
+            <div style={{ outline: "solid 1px red" }}>
+              <Text {...i.combination}>
+                <Skeleton variation="text" />
+              </Text>
+            </div>
+          </Grid.Column>
+          <Grid.Column size={4}>
+            <div style={{ outline: "solid 1px blue" }}>
+              <Text {...i.combination}>
+                {capitalCase(i.combination.variation ?? "-")}
+                <Skeleton variation="text" />
+              </Text>
+            </div>
+          </Grid.Column>
+        </React.Fragment>
+      ))}
+    </Grid>
+  ),
+};
--- packages/docs/storybook/.storybook/preview.ts
+++ packages/docs/storybook/.storybook/preview.ts
@@ -34,6 +34,7 @@ const preview: Preview = {
           "*",
           "Layout",
           "Utility",
+          "Test",
         ],
       },
     },
