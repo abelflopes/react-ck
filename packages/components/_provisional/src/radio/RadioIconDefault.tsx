17f1593e7a1a56fe0c36a8d800e8e8cb401f3f21
Abel <abelflopes@gmail.com>
2024-09-23 19:18:47 +0100

    fix: boolean input default icon components

--- packages/components/_provisional/src/radio/RadioIconDefault.tsx
+++ packages/components/_provisional/src/radio/RadioIconDefault.tsx
@@ -1,13 +1,9 @@
 import styles from "./styles/icon.module.scss";
 import React from "react";
-import classNames from "classnames";
 import { type BooleanInputIconComponent } from "../boolean-input";
 
-export const RadioIconDefault: BooleanInputIconComponent = ({ checked }) => (
-  <span
-    className={classNames(styles.root, {
-      [`${styles.checked}`]: checked,
-    })}>
+export const RadioIconDefault: BooleanInputIconComponent = () => (
+  <span className={styles.root}>
     <span className={styles.icon} />
   </span>
 );
