733b19f7bb4e3a6fdcc52274d62c89ef8165cec1
Abel <abelflopes@gmail.com>
2024-10-09 21:17:50 +0100

    fix(input): ref handling

--- packages/components/input/src/index.tsx
+++ packages/components/input/src/index.tsx
@@ -1,5 +1,6 @@
 import React, {
   type ComponentPropsWithRef,
+  forwardRef,
   type HTMLInputTypeAttribute,
   useEffect,
   useMemo,
@@ -8,7 +9,7 @@ import defaultStyles from "./styles/default.module.scss";
 import { useFormFieldContext, type FormFieldContextProps } from "@react-ck/form-field";
 import classNames from "classnames";
 
-export interface InputProps extends Omit<ComponentPropsWithRef<"input">, "children"> {
+interface InputProps extends Omit<ComponentPropsWithRef<"input">, "children"> {
   skin?: FormFieldContextProps["skin"];
 }
 
@@ -18,49 +19,53 @@ export interface InputProps extends Omit<ComponentPropsWithRef<"input">, "childr
  * @returns a React element
  */
 
-export const Input = ({
-  skin,
-  id,
-  className,
-  ...props
-}: Readonly<InputProps>): React.ReactElement => {
-  const formFieldContext = useFormFieldContext();
+const Input = forwardRef<HTMLInputElement, Readonly<InputProps>>(
+  ({ skin, id, className, ...props }, ref): React.ReactElement => {
+    const formFieldContext = useFormFieldContext();
 
-  const computedSkin = useMemo(
-    () => formFieldContext?.skin ?? skin ?? "default",
-    [formFieldContext?.skin, skin],
-  );
+    const computedSkin = useMemo(
+      () => formFieldContext?.skin ?? skin ?? "default",
+      [formFieldContext?.skin, skin],
+    );
 
-  const computedId = useMemo(() => formFieldContext?.id ?? id, [formFieldContext?.id, id]);
+    const computedId = useMemo(() => formFieldContext?.id ?? id, [formFieldContext?.id, id]);
 
-  // Determines if input should render the default styles or not
-  const isDefaultStyle = useMemo(() => {
-    const specialTypes: HTMLInputTypeAttribute[] = ["checkbox", "radio", "range", "color"];
-    return !props.type || !specialTypes.includes(props.type);
-  }, [props.type]);
+    // Determines if input should render the default styles or not
+    const isDefaultStyle = useMemo(() => {
+      const specialTypes: HTMLInputTypeAttribute[] = ["checkbox", "radio", "range", "color"];
+      return !props.type || !specialTypes.includes(props.type);
+    }, [props.type]);
 
-  // Validate usage inside form field
-  useEffect(() => {
-    // Is not inside form field, skip
-    if (formFieldContext === undefined) return;
+    // Validate usage inside form field
+    useEffect(() => {
+      // Is not inside form field, skip
+      if (formFieldContext === undefined) return;
 
-    // Is inside form field
-    if (skin) throw new Error("When using input inside form field, define skin on the form field");
-    else if (id) throw new Error("When using input inside form field, define id on the form field");
-  }, [formFieldContext, id, skin]);
+      // Is inside form field
+      if (skin)
+        throw new Error("When using input inside form field, define skin on the form field");
+      else if (id)
+        throw new Error("When using input inside form field, define id on the form field");
+    }, [formFieldContext, id, skin]);
 
-  return (
-    <input
-      {...props}
-      id={computedId}
-      className={classNames(
-        isDefaultStyle && [
-          defaultStyles.root,
-          formFieldContext === undefined && defaultStyles.standalone,
-          defaultStyles[`skin_${computedSkin}`],
-        ],
-        className,
-      )}
-    />
-  );
-};
+    return (
+      <input
+        ref={ref}
+        {...props}
+        id={computedId}
+        className={classNames(
+          isDefaultStyle && [
+            defaultStyles.root,
+            formFieldContext === undefined && defaultStyles.standalone,
+            defaultStyles[`skin_${computedSkin}`],
+          ],
+          className,
+        )}
+      />
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export { Input, type InputProps };
