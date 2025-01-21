3cbade4a2709da8e8aefe4a738d931eb66e7d609
Abel <abel@synthexhq.com>
2025-01-21 18:11:39 +0000

    fix(breadcrumbs): align icon

--- packages/docs/stories/src/generic-breadcrumbs.stories.tsx
+++ packages/docs/stories/src/generic-breadcrumbs.stories.tsx
@@ -4,6 +4,8 @@ import { Manager } from "@react-ck/manager";
 import { configureStory } from "@react-ck/story-config";
 import { Breadcrumbs } from "@react-ck/provisional/src";
 import { faker } from "@faker-js/faker";
+import { Icon } from "@react-ck/icon/src";
+import { IconCog } from "@react-ck/icon/icons/IconCog";
 
 type Story = StoryObj<typeof Breadcrumbs>;
 
@@ -27,6 +29,9 @@ export const Component: Story = {
     items: [
       <Breadcrumbs.Item key="1" as={<a href="/"> {faker.lorem.word()}</a>} />,
       <Breadcrumbs.Item key="2" as={["a", { href: "../" }]}>
+        <Icon size="text">
+          <IconCog />
+        </Icon>
         {faker.lorem.word()}
       </Breadcrumbs.Item>,
       <Breadcrumbs.Item key="3" active>
